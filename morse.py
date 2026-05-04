e344a46ab7a950d78ffb0ca96a35769ea9530000
KevXveK <kevxlearn@gmail.com>
2026-05-04 11:48:49 -0300

    feat(accents): expand morse code table to include accented letters

--- morse.py
+++ morse.py
@@ -1,4 +1,4 @@
-codigo_morse = { #alfabeto,numeros e simbolos em python  
+codigo_morse = {   
     'A': '.-',    'B': '-...',  'C': '-.-.',  'D': '-..',   'E': '.',    
     'F': '..-.',  'G': '--.',   'H': '....',  'I': '..',    'J': '.---', 
     'K': '-.-',   'L': '.-..',  'M': '--',    'N': '-.',    'O': '---',  
@@ -9,21 +9,25 @@ codigo_morse = { #alfabeto,numeros e simbolos em python
     '9': '----.',  ', ': '--..--', '.': '.-.-.-', '?': '..--..', '/': '-..-.', 
     '-': '-....-', '(': '-.--.',  ')': '-.--.-', '&': '.-...', ':': '---...', 
     ';': '-.-.-.', '=': '-...-',  '+': '.-.-.',  '_': '..--.-', '"': '.-..-.', 
-    '$': '...-..-', '@': '.--.-.'
+    '$': '...-..-', '@': '.--.-.', ' ': '/', 'Á': '.--.-', 'É': '..-..','È': '.-..-', 'Í': '..--.', 'Ó': '---.', 'Ú': '..--'
 }
-def conversor_morse(): # função que traduz texto para morse e morse para texto
-    resposta = int(input("Digite o numero 1 para traduzir texto para morse ou 2 para morse para texto: ")) # pede uma escolha para o usuario
+def conversor_morse(): 
+    resposta = int(input("Digite o numero 1 para traduzir converter para morse ou 2 para morse para texto: ")) # pede uma escolha para o usuario
     traducao = []                                                                        # lista que vai ser armazenado a tradução
 
     if resposta == 1:
         texto = input(f"Digite o texto  que deseja transformar em codigo morse: ").upper()
         for letra in texto:
+            # if letra == ' ':
+                # traducao.append(' ')  # se o caracter for um espaço, adiciona um espaço na tradução
             if letra in codigo_morse:
                 traducao.append(codigo_morse[letra])  #cria um loop que itera sobre cada caracter e devolve o valor da chave desse caracter
         return ' '.join(traducao) # retorna a variavel como uma string
     elif resposta == 2:
-        texto = input("digite o codigo morse separado por espaço").split()
+        texto = input("digite o codigo morse separado por espaço : ").split()
         for cod in texto:
+            # print("esse é a variavel cod do input do usuario: " + cod)
+            # print("esse é o valor da variavel texto: " + str(texto))
             for chave,valor in codigo_morse.items():
                 if cod == valor:
                     traducao.append(chave)
